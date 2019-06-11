fa4494c53cad6f172264bf4be68afae51c3566e4
isaac1104 <isaac-kwon@hotmail.com>
2019-06-11 13:26:20 +0900

    styling change & landing text

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import { Col } from 'antd';
+import { Col, Typography } from 'antd';
 import MenuCard from '../MenuCard/MenuCard';
 import styles from './Home.module.css';
 import burgers from '../../assets/burgers.png';
 import sides from '../../assets/sides.png';
 import drinks from '../../assets/drinks.png';
 
+const { Title } = Typography;
+
 const Home = () => {
   return (
     <div className={styles.HomeContainer}>
-      <Col xs={24} sm={24} md={24} lg={8} xl={8}>
-        <MenuCard 
-        menu='Burgers'
-        description='Our delicous burgers!'  
-        img={burgers}
-        />
-      </Col>
-      <Col xs={24} sm={24} md={24} lg={8} xl={8}>
-        <MenuCard
-          menu='Sides'
-          description='Great sides to go along with your burgers!'
-          img={sides}
-        />
-      </Col>
-      <Col xs={24} sm={24} md={24} lg={8} xl={8}>
-        <MenuCard
-          menu='Drinks'
-          description='Refreshing drinks!'
-          img={drinks}
-        />
-      </Col>
+      <div>
+        <Title>Welcome To McBurgerKing</Title>
+        <Title level={3}>Where taste is king!</Title>
+      </div>
+      <div>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <MenuCard
+            menu='Burgers'
+            description='Check out our delicous burgers!'
+            img={burgers}
+          />
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <MenuCard
+            menu='Sides'
+            description='Great sides to go along with your burgers!'
+            img={sides}
+          />
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <MenuCard
+            menu='Drinks'
+            description='Refreshing drinks to quench your thirst!'
+            img={drinks}
+          />
+        </Col> 
+      </div>
     </div>
   );
 }
