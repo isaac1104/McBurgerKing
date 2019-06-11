75d9954a461a8d6cb799a308026c8f0a5986d863
isaac1104 <isaac-kwon@hotmail.com>
2019-06-11 17:46:38 +0900

    fetch data for one menu

--- routes/menu_data_route.js
+++ routes/menu_data_route.js
@@ -10,4 +10,14 @@ module.exports = app => {
       res.status(400).send(error)
     }
   });
+
+  app.get('/api/data/:id', async (req, res) => {
+    try {
+      const request = await axios.get(`https://api-mcburgerking.herokuapp.com/${req.params.id}`);
+      const { data } = request;
+      res.status(200).send(data);
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  });
 };
