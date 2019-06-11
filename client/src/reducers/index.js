36e69b0d1cb8b35a808c1beea775ced92d85c7e5
isaac1104 <isaac-kwon@hotmail.com>
2019-06-11 17:47:50 +0900

    add menu reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import allMenusReducer from './all_menus_reducer';
+import menuReducer from './menu_reducer';
 import uiVisibilityReducer from './ui_visibility_reducer';
 
 const rootReducer = combineReducers({
   menus: allMenusReducer,
+  menu: menuReducer,
   ui_visibility: uiVisibilityReducer
 });
 
