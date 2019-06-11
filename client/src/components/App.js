82ed70838cb3714162013ec36e5098a321bf211e
isaac1104 <isaac-kwon@hotmail.com>
2019-06-11 16:07:33 +0900

    fetch menu data on app mounts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { fetchMenuData } from '../actions';
 import Home from './Home/Home';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchMenuData();
+  }
+  
   render() {
     return (
       <BrowserRouter>
@@ -14,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, { fetchMenuData })(App);
