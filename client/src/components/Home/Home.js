240218d6376b14645e7d835209bb3decc0341f68
isaac1104 <isaac-kwon@hotmail.com>
2019-06-11 13:11:51 +0900

    add menu cards

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Col } from 'antd';
+import MenuCard from '../MenuCard/MenuCard';
 import styles from './Home.module.css';
+import burgers from '../../assets/burgers.png';
+import sides from '../../assets/sides.png';
+import drinks from '../../assets/drinks.png';
 
 const Home = () => {
   return (
     <div className={styles.HomeContainer}>
-      <h1>Home</h1>
+      <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+        <MenuCard 
+        menu='Burgers'
+        description='Our delicous burgers!'  
+        img={burgers}
+        />
+      </Col>
+      <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+        <MenuCard
+          menu='Sides'
+          description='Great sides to go along with your burgers!'
+          img={sides}
+        />
+      </Col>
+      <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+        <MenuCard
+          menu='Drinks'
+          description='Refreshing drinks!'
+          img={drinks}
+        />
+      </Col>
     </div>
   );
 }
---
+++ client/src/components/MenuCard/MenuCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card } from 'antd';
+import styles from './MenuCard.module.css';
+
+const { Meta } = Card;
+
+const MenuCard = ({ menu, description, img }) => {
+  return (
+    <Card
+      hoverable
+      className={styles.MenuCard}
+      cover={<img alt='example' src={img} />}
+    >
+      <Meta title={menu} description={description} />
+    </Card>
+  );
+}
+
+MenuCard.propTypes = {
+  menu: PropTypes.string,
+  description: PropTypes.string,
+  img: PropTypes.string
+};
+
+export default MenuCard;
