4e65d122927f4929be7198e150bf6ce856368432
isaac1104 <isaac-kwon@hotmail.com>
2019-06-11 15:57:37 +0900

    add action creators for fetching menu data

---
+++ client/src/actions/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { FETCH_MENU_DATA_REQUEST, FETCH_MENU_DATA_SUCCESS, FETCH_MENU_DATA_FAIL } from './types';
+
+const fetchDataRequest = () => ({
+  type: FETCH_MENU_DATA_REQUEST,
+  payload: true
+});
+
+const fetchDataSuccess = data => ({
+  type: FETCH_MENU_DATA_SUCCESS,
+  payload: data
+});
+
+const fetchDataFail = error => ({
+  type: FETCH_MENU_DATA_FAIL,
+  payload: error
+});
+
+export const fetchMenuData = () => async dispatch => {
+  dispatch(fetchDataRequest());
+  try {
+    const request = await axios.get('/api/data');
+    const { data } = request;
+    dispatch(fetchDataSuccess(data));
+  } catch (error) {
+    dispatch(fetchDataFail(error));
+  }
+};
