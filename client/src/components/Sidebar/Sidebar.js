85405344593027efe6a71373feaa4878732b46f6
isaac1104 <isaac-kwon@hotmail.com>
2019-06-12 23:02:27 +0900

    calculate & show total price

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { removeItemFromCart, toggleSidebar } from '../../actions';
 import styles from './Sidebar.module.css';
 
+const { Title } = Typography;
+
 class Sidebar extends Component {
   renderCartItems() {
     const { data } = this.props.cart_items;
@@ -28,19 +30,34 @@ class Sidebar extends Component {
     ));
   }
 
+  renderTotalPrice() {
+    const { data } = this.props.cart_items;
+    return (
+      <div className={styles.CartItemTotalContainer}>
+        <div className={styles.CartItemTotal}>
+          <Title level={3}>TOTAL: </Title>
+          <Title code level={3}>${this.calculateTotalPrice(data)}</Title>
+        </div>
+      </div>
+    )
+  }
+
+  calculateTotalPrice(data) {
+    return data.reduce((acc, curr) => curr.price * curr.quantity + acc, 0).toFixed(2);
+  }
+
   render() {
-    console.log(this.props.cart_items.data);
     const { ui_visibility: { sidebar_visible }, toggleSidebar } = this.props;
     return (
       <Drawer
-        title='My Meal'
+        title='MY MEAL'
         placement='left'
-        closable={false}
         width={350}
         onClose={toggleSidebar}
         visible={sidebar_visible}
       >
         {this.renderCartItems()}
+        {this.renderTotalPrice()}
       </Drawer>
     );
   }
